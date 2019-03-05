63bb40b87dfd50cbc502903dc123025977f90998
Kondaiah Y <k.yallavula@gmail.com>
2019-03-05 18:58:27 +0530

    Fifth commit

--- src/main/java/com/inventory/InventoryApp/InventoryAppApplication.java
+++ src/main/java/com/inventory/InventoryApp/InventoryAppApplication.java
@@ -17,6 +17,8 @@ public class InventoryAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryAppApplication.class, args);
 		System.out.println("Sample Test Runnig in test environment");
+		System.out.println("Sample Test Runnig in test environment");
+		System.out.println("Sample Test Runnig in test environment");
 	}
 
 }
